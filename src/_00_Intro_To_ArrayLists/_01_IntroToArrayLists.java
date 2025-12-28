902d6ec657771f0b626f963a0f86300228802ebf
league <league@192.168.1.78>
2025-12-27 16:25:47 -0800

    finisshed this going ot finish more soon

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,21 +1,41 @@
 package _00_Intro_To_ArrayLists;
+import java.util.ArrayList;
 
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
         // 1. Create an array list of Strings
+    	ArrayList<String> words = new ArrayList<>();
         //    Don't forget to import the ArrayList class
 
         // 2. Add five Strings to your list
-
+    	words.add("Bread");
+    	words.add("Planes");
+    	words.add("Apple");
+    	words.add("Bacon");
+    	words.add("Mint");
         // 3. Print all the Strings using a standard for-loop
-
+    	for (int i = 0; i <words.size();i ++) {
+    		System.out.println(words.get(i));
+    	}
         // 4. Print all the Strings using a for-each loop
-
+    	for (String word : words) {
+    		System.out.println(word);
+    	}
         // 5. Print only the even numbered elements in the list.
-
+    	for (int i = 0; i <words.size(); i +=2) {
+    		System.out.println(words.get(i));
+    	}
+    		
         // 6. Print all the Strings in reverse order.
-
+    	for (int i = words.size() - 1; i >= 0; i--) {
+    		System.out.println(words.get(i));
+    	}
         // 7. Print only the Strings that have the letter 'e' in them.
-        
+        for (String word: words) {
+        	if (word.contains("e")) {
+        		System.out.println(word);
+        	}
+        	
+        }
     }
 }
